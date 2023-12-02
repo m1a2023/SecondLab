1d351a340e7df28d83da63383168601965e9b8be
m1a <mag2017go@gmail.com>
2023-12-02 17:52:45 +0500

    finally?

---
+++ secondLab/MethodsSecondLab.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecondLab;
+
+class Methods
+{
+	public static List<string> GetList(string input)
+	{
+		int i = 0; string box = "";
+		List<string> output = new List<string>();
+
+		while (i != input.Length)
+		{
+			if (char.IsDigit(input[i]))
+				box += input[i];
+
+			else
+			{
+				output.Add(box);
+
+				if (input[i] == ' ') continue;
+				else
+					output.Add(input[i].ToString());
+				box = "";
+			}
+			i++;
+		}
+		if (!string.IsNullOrEmpty(box))
+			output.Add(box);
+		return output;
+	}
+
+	public static double GetResult(List<string> input)
+	{
+		List<string> operators = GetListOperators(input);
+		List<double> digits = GetListDigits(input);
+		int j = 0; double result = 0d;
+		while (j < operators.Count)
+		{
+
+			if (operators[j] == "*")
+			{
+				result = Calculate(digits[j], digits[j + 1], operators[j]);
+				digits[j] = result;
+				digits.RemoveAt(j + 1);
+				operators.RemoveAt(j);
+			}
+			else if (operators[j] == "/")
+			{
+				if (digits[j + 1] != 0)
+				{
+					result = Calculate(digits[j], digits[j + 1], operators[j]);
+					digits[j] = result;
+					digits.RemoveAt(j + 1);
+					operators.RemoveAt(j);
+				}
+			}
+			else j++;
+		}
+
+		j = 0;
+		while (j < operators.Count)
+		{
+			if (operators[j] == "+")
+			{
+				result = Calculate(digits[j], digits[j + 1], operators[j]);
+				digits[j] = result;
+				digits.RemoveAt(j + 1);
+				operators.RemoveAt(j);
+			}
+			else if (operators[j] == "-")
+			{
+				result = Calculate(digits[j], digits[j + 1], operators[j]);
+				digits[j] = result;
+				digits.RemoveAt(j + 1);
+				operators.RemoveAt(j);
+			}
+			else j++;
+		}
+
+		return digits[^1];
+	}
+
+	static double Calculate(double a, double b, string sym)
+	{
+		return sym switch
+		{
+			"*" => a * b,
+			"/" => a / b,
+			"+" => a + b,
+			"-" => a - b
+		};
+	}
+
+	static int GetHighestPriorityIndex(List<string> input)
+	{
+
+		for (int i = 0; i < input.Count; i++)
+		{
+			if (input[i] == "*" || input[i] == "/")
+			{
+				return i;
+			}
+			else if (input[i] == "+" || input[i] == "-")
+				return i;
+		}
+		return -1;
+	}
+
+	static List<string> GetListOperators(List<string> input)
+	{
+		List<string> listSymbolOutput = new List<string>();
+
+		foreach (string element in input)
+		{
+			if (element == "*" || element == "/" || element == "+" || element == "-")
+			{
+				listSymbolOutput.Add(element);
+			}
+		}
+
+		return listSymbolOutput;
+	}
+
+	static List<double> GetListDigits(List<string> input)
+	{
+		List<double> output = new List<double>();
+
+		foreach (string element in input)
+		{
+			if (double.TryParse(element, out double result))
+				output.Add(result);
+		}
+
+		return output;
+	}
+}
--- secondLab/Program.cs
+++ secondLab/Program.cs
@@ -1,65 +1,21 @@
-﻿class Solution
+﻿using System.Linq.Expressions;
+
+namespace SecondLab;
+
+class Output
 {
     static void Main()
     {
         string input = Console.ReadLine();
-        Calculate(input);
-    }
 
-    public static void Calculate(string input)
-    {
-        List<double> Digits = new List<double>();
-        List<char> Symbols = new List<char>();
-        string timeBox = "", updateTime = "";
-        char space = ' ';
-
-        for (int i = 0; i < input.Length; ++i)
-        {
-            if (char.IsDigit(input[i]))
-                timeBox += input[i];
-            else
-            {
-                if (input[i] != space)
-                {
-                    Digits.Add(double.Parse(timeBox));
-                    Symbols.Add(input[i]);
-                    timeBox = updateTime;
-                }
-
-            }
-        }
-        if (!string.IsNullOrEmpty(timeBox))
-            Digits.Add(double.Parse(timeBox));
-
-        double result = 0d, timeDigitBox = 0d, emptyBox = 0d;
-        List<double> newDigits = new List<double>(Digits);
-        int j = 0;
-        double output;
-        while (j < Symbols.Count)
-        {
-            switch (Symbols[j])
-            {
-                case '+':
-                    timeDigitBox += newDigits[j] + newDigits[j + 1];
-                    break;
-                case '-':
-                    timeDigitBox += newDigits[j] - newDigits[j + 1];
-                    break;
-                case '*':
-                    timeDigitBox += newDigits[j] * newDigits[j + 1];
-                    break;
-                case '/':
-                    timeDigitBox += newDigits[j] / newDigits[j + 1];
-                    break;
-            }
-            newDigits[j + 1] = timeDigitBox;
-            timeDigitBox = emptyBox;
-            j++;
-
-        }
-        output = newDigits[^1];
+        List<string> listInput = new List<string>();
+        listInput = Methods.GetList(input);
 
 
-        Console.WriteLine(output);
+        foreach(string j in listInput) { Console.Write(j, " "); }
+        Console.WriteLine();
+
+        double result = Methods.GetResult(listInput);
+        Console.WriteLine(result);
     }
 }
